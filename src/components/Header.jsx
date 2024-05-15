970fce3fee1acb3e68518af348a450231d1ea3b0
Juan Jose Romero Montes <claseinfojuanjose@gmail.com>
2024-05-15 13:19:12 +0200

    Feature: Header component

---
+++ src/components/Header.jsx
@@ -0,0 +1,11 @@
+import './Header.css'
+
+const Title = () => {
+  return (
+    <div class="header">
+        <h1 class="header__text">Rick & Morty Characters</h1>
+    </div>
+  )
+}
+
+export default Title;
